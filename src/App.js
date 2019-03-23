41b5d28df3f0ff72cc155f4bd0154c74a59528a6
FernandoGalende <fernandogalende@gmail.com>
2019-03-23 13:04:43 +0100

    Create components, use props and actualice the state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Text from './text.componet'
+
+class Contador extends Component {
+  constructor(){
+    super()
+    this.state = {contador: 1}
+   setInterval (() => {
+     this.setState({
+      contador: this.state.contador +1
+    })
+    },1000)
+  }
+  render(){
+    return(
+      <h1>{this.state.contador}</h1>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <Contador/>
+          <Text
+            arrayOfNumbers={[1,2,3,4]}
+            objetEjemplo={
+              {
+                key1: 'Value1',
+                key2: 'Value2'
+              }
+            }
+            multiply={(number) => number*2}
+            title="Component Text"
+            value={0}
+          />
+
         </header>
       </div>
     );
